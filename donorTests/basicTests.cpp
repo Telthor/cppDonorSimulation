f52568df10c7321d8c3a9a9f20fba3820f190d66
David Wise <d.wise.15@ucl.ac.uk>
2017-03-30 15:08:30 +0100

    Added google test functionality

---
+++ donorTests/basicTests.cpp
@@ -0,0 +1,10 @@
+//
+// Created by David Wise on 30/03/2017.
+//
+
+#include "gtest/gtest.h"
+
+
+TEST(basic_test, test_eq) {
+    EXPECT_EQ(1, 1);
+}
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include "donorClass.h"
 #include <stdio.h>
 #include <Eigen/Dense>
-#include "gnuplot-iostream/gnuplot-iostream.h"
+//#include "gnuplot-iostream/gnuplot-iostream.h"
 
 int main() {
 	Donor phos(0.5, 7.29e-26);
@@ -23,6 +23,23 @@ int main() {
 	ComplexEigenSolver<MatrixXcd> ces;
     ces.compute(phos.Ham);
 	std::cout << "Eigs are: \n" << phos.getEigs(0.0005) << "\n";
+
+//    Gnuplot gp;
+//    std::vector<std::complex<double>> eig1;
+//    std::vector<std::complex<double>> eig2;
+//    std::vector<std::complex<double>> eig3;
+//    std::vector<std::complex<double>> eig4;
+//    std::vector<double> fields;
+//
+//    for (double incr = 0; incr <0.1; incr += 0.001) {
+//        eig1.push_back(phos.getEigs(incr)[0]);
+//        eig2.push_back(phos.getEigs(incr)[1]);
+//        eig3.push_back(phos.getEigs(incr)[2]);
+//        eig4.push_back(phos.getEigs(incr)[3]);
+//        fields.push_back(incr);
+//    }
+
+
 //    std::cout << "Sy is \n" << phos.Sy << "\n";
 //    std::cout << "Cr is \n" << phos.Icr << "\n";
 //    std::cout << "An is \n" << phos.Ian << "\n";
